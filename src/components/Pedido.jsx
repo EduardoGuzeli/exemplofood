3fbe1997cc76afba254c52d1bde7419bdbfd103b
Eduardo <eduguzeli@gmail.com>
2026-09-18 09:16:59 -0300

    feat: funções do pedido

--- src/components/Pedido.jsx
+++ src/components/Pedido.jsx
@@ -9,6 +9,52 @@ const cardapio=[
 ];
 
 const Pedido = () => {
+
+  // HOOK - useState- Manipula o estado da variavel 
+  // Estados para gerenciar a lista de items do cardápio
+  const[items,setItems]=useState(cardapio);
+  const[status,setStatus]=useState("");
+  const[enviar,setEnviar]=useState(false);
+
+  // Valor fixo adicionado ao total quando tiver itens do carrinho
+  const taxaEntrega=5.00;
+
+  // Função que altera a quantidade do pedido 
+  const AlterarQuantidade =(id,valor)=>{
+    setItems(alt =>
+      // MAP: Cria um novo array e percorre os items sem modificar o original(IMUTABILIDADE)
+      alt.map(item=>
+        // TERNARIO: Verifica seu item da iteração atual é o que deve ser alterado 
+        // SPRED (...item): Mantem os dados antigos e adiciona os novos
+        // MATH.MAX: Objeto que garante que a quantidade nunca seja menor que 0
+        item.id === id ? {...item,quantidade: Math.Max(0,item.quantidade + valor)}:item
+      )
+    )
+  }
+
+  // FILTER: Seleciona apenas os produtos disponiveis do carrinho
+  const produtosDisponiveis = items.filter(item =>item.disponivel);
+  const carrinho = items.filter(item => item.quantidade > 0);
+
+  // REDUCE: Calcula a soma dos itens (Preco + quantidade) e adiciona a taxa de entrega 
+  const subTotal = carrinho.reduce((ac,item)=> ac + item.preco * item.quantidade,0);
+  const total =subTotal >0 ? subTotal + taxaEntrega: 0;
+
+  //SIMULAÇÃO DO CICLO DE VIDA DE ENTREGA USANDO TEMPORIZADOR ASSÍNCRONO
+  const ConfirmarPedido=()=>{
+    setEnviar(true);
+    setStatus("Restaurante Confirmou pagamento, preparando pedido");
+    setTimeout(()=> {
+      setStatus("Seu pedido saiu para entrega")
+      setEnviar(false);
+    },5000)
+    setTimeout(()=>{
+      setStatus("Seu pedido foi entregue com sucesso");
+      setEnviar(false)
+    },10000)
+  }
+
+
   return (
     <>
       
